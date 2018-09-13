59905148fc074fa95ba9a651bcd87f5839f7a79e
edogbosunny <edogbosunday@gmail.com>
2018-09-13 12:56:39 +0100

    configure usertable seed

---
+++ models/users.js
@@ -0,0 +1,24 @@
+import db from "../config/db";
+
+//create users table
+
+const createUsersTable = async () => {
+  const client = await db.connect();
+  try {
+    const query = `CREATE TABLE IF NOT EXISTS users (
+          user_id SERIAL UNIQUE,
+          username VARCHAR NOT NULL,
+          email VARCHAR NOT NULL,
+          password VARCHAR NOT NULL, 
+          user_role VARCHAR NOT NULL, 
+          PRIMARY KEY(user_id, email)
+      );`;
+
+    await client.query(query);
+  } catch (e) {
+    throw e;
+  } finally {
+    client.release();
+  }
+};
+export default createUsersTable;
