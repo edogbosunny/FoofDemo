874d17f784715478168b0e77a4a45648dad72c82
edogbosunny <edogbosunday@gmail.com>
2018-09-13 12:41:05 +0100

    configure pg-pool

---
+++ config/db.js
@@ -0,0 +1,39 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import dbURI from "./keys";
+
+const connectionString = dbURI.psqlURI || process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString
+});
+
+/**
+ * Stop application an error occurs on the Database
+ */
+pool.on("error", err => {
+  console.error("Unexpected error on idle client", err.stack);
+  pool.end();
+  process.exit(-1);
+});
+
+/**
+ * When the Database is connected
+ */
+pool.on("connect", () => {
+  console.log("PostgreSQL Database Connected");
+});
+
+process.on("SIGINT", () => {
+  console.log(" Closing PostgreSQL Database ....");
+  pool
+    .end()
+    .then(() => {
+      console.log("Database Succesfully Closed");
+    })
+    .catch(err => {
+      console.log(err.stack);
+    });
+});
+
+export default pool;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  psqlURI:
+    "postgres://qhefalxv:38KtNbxAYz_ugrv_96XyjZrVXlaUkaEr@stampy.db.elephantsql.com:5432/qhefalxv"
+};
