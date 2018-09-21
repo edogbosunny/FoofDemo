5c235701470a647c504cb004010bbf402b68cbce
edogbosunny <edogbosunday@gmail.com>
2018-09-21 16:10:14 +0100

    maintainability edit

--- server/controllers/signinController.js
+++ server/controllers/signinController.js
@@ -9,55 +9,32 @@ class signin {
     //  console.log(req.app.get("user_id"));
     const { email, password } = req.body;
     const { errors, isValid } = validateSignin(req.body);
-    if (!isValid) {
-      return res.status(400).json({
-        status: "failed",
-        token: null,
-        error: errors
-      });
+    if (!isValid) { return res.status(400).json({
+        status: "failed", token: null, error: errors });
     } else {
       (async () => {
         try {
-          const user = await db.query("SELECT * FROM users WHERE email = $1", [
-            email
-          ]);
+          const user = await db.query("SELECT * FROM users WHERE email = $1", [ email ]);
           if (user.rows.length < 1) {
             res.status(401).json({
-              auth: false,
-              token: null,
-              message: "User does not exist"
+              auth: false, token: null, message: "User does not exist"
             });
           } else if (!bcrypt.compareSync(password, user.rows[0].hashpassword)) {
-            res.status(401).json({
-              auth: false,
-              token: null,
-              message: "Password is not correct"
-            });
+            res.status(401).json({ auth: false, token: null, message: "Password is not correct" });
           } else {
             const userId = user.rows[0].user_id;
             // console.log("user table", user);
-            const token = jwt.sign({ id: userId }, config.tokenSecret, {
-              expiresIn: 86400
-            });
-            res.status(200).json({
-              auth: true,
-              message: "Login Successful",
-              token
-            });
+            const token = jwt.sign({ id: userId }, config.tokenSecret, { expiresIn: 86400  });
+            res.status(200).json({ auth: true, message: "Login Successful", token });
           }
         } catch (e) {
           throw e;
         }
       })().catch(err => {
         console.error(err);
-        res.status(500).json({
-          auth: false,
-          token: null,
-          messsage: "The Server encountered a problem"
-        });
+        res.status(500).json({ auth: false, token: null, messsage: "The Server encountered a problem" });
       });
     }
   }
 }
-
 export default signin;
