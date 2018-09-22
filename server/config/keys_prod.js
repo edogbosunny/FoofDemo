d3a12aa0c0c9524d3c09f8bbaf3ee71fe32d34ad
edogbosunny <edogbosunday@gmail.com>
2018-09-22 20:47:33 +0100

    :rocket: chai test writing

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -2,10 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pgURI = {
-  // psqlURI: process.env.PSQL_URI
-  psqlURI:
-    "postgres://qhefalxv:38KtNbxAYz_ugrv_96XyjZrVXlaUkaEr@stampy.db.elephantsql.com:5432/qhefalxv"
+module.exports = {
+  psqlURI: process.env.PSQL_URI
 };
-
-export default { pgURI };
